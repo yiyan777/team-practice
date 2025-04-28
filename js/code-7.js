e5f95c555ae62dbceb74d873545fc4f9392f7462
wangyiyan <achievement1993@gmail.com>
2025-04-28 15:27:43 +0800

    調整文字在上 圖片在下

--- js/code-7.js
+++ js/code-7.js
@@ -1,9 +1,12 @@
 const user7 = document.querySelector(".user-7");
 const user7Img = document.createElement("img");
+const user7Text = document.createElement("div");
 
-user7.textContent = "痛みを感じる 神羅關稅!";
+user7Text.textContent = "痛みを感じる 神羅關稅!";
+user7Text.classList.add(".text");
 
 user7Img.src = "https://images.chinatimes.com/newsphoto/2025-03-24/1024/20250324004771.jpg";
 user7Img.style.width = "300px";
 
+user7.appendChild(user7Text);
 user7.appendChild(user7Img);
