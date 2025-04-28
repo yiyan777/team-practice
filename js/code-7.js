6dbe46ffd7a462f6bf5bb003804283388bd67ddc
wangyiyan <achievement1993@gmail.com>
2025-04-28 15:56:30 +0800

    新增flex

--- js/code-7.js
+++ js/code-7.js
@@ -2,6 +2,7 @@ const user7 = document.querySelector(".user-7");
 const user7Img = document.createElement("img");
 const user7Text = document.createElement("div");
 
+user7.textContent = "";
 user7Text.textContent = "痛みを感じる 神羅關稅!";
 user7Text.classList.add(".text");
 
@@ -10,3 +11,7 @@ user7Img.style.width = "300px";
 
 user7.appendChild(user7Text);
 user7.appendChild(user7Img);
+
+user7Text.addEventListener("click", () => {
+    window.open("https://tw.stock.yahoo.com/quote/2330.TW","_blank");
+});
